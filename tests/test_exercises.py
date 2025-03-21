991dff95a8776223931bf79648ba0c1bd81aabae
MarlonBando <michele.bandini2000@gmail.com>
2025-03-21 11:11:52 +0100

    added tests

---
+++ tests/test_exercises.py
@@ -0,0 +1,62 @@
+import os
+import pandas as pd
+
+# TODO: add import for the function
+
+
+def test_exercise_1():
+    run_test("ex1")
+
+def test_exercise_2():
+    run_test("ex2")
+
+def test_exercise_3():
+    run_test("ex3")
+
+def run_test(filename:str):
+    solution_file = os.path.join(get_res_path(),"solutions",f"{filename}_s.csv")
+
+    input_file = os.path.join(get_res_path(),"inputs",f"{filename}.csv")
+    output_file = os.path.join(get_tmp_path(),f"{filename}_s.csv")
+
+    # TODO: call the function to test
+
+    output_df = pd.read_csv(output_file)
+    solution_df = pd.read_csv(solution_file)
+
+    print(f"Output dataframe shape: {output_df.shape}")
+    print(f"Solution dataframe shape: {solution_df.shape}")
+    if not output_df.shape == solution_df.shape:
+        print("Error: dataframe shape are different")
+        assert(False)
+
+    print("Output dataframe:")
+    print(output_df)
+    print("==============================")
+
+    print("Solution dataframe:")
+    print(solution_df)
+    print("==============================")
+    assert(output_df.equals(solution_df))    
+
+def get_root():
+    '''
+    Get the project root
+    '''
+    return os.path.dirname(os.path.dirname(__file__))
+
+def get_res_path():
+    '''
+    Get the absoulte path of the tests/res folder
+    '''
+    return os.path.join(get_root(),"tests","res")
+
+def get_tmp_path():
+    '''
+    Get the absoulte path of the tests/.tmp folder.
+    If it doesn't exist it creates it
+    '''
+    tmp_path = os.path.join(get_root(), "tests", ".tmp")
+    if not os.path.exists(tmp_path):
+        os.makedirs(tmp_path)
+    return tmp_path
